8fa4f1c9710c492ba23cd0a83f48192fa6d56e84
grey <charminggw@gmail.com>
2024-02-20 13:32:08 +0900

    feat(proxy): 다이내믹 프록시 적용

--- src/test/java/org/springbook/learningtest/jdk/HelloTest.java
+++ src/test/java/org/springbook/learningtest/jdk/HelloTest.java
@@ -3,6 +3,7 @@ package org.springbook.learningtest.jdk;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.reflect.Proxy.newProxyInstance;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HelloTest {
@@ -21,6 +22,18 @@ class HelloTest {
         assertThat( hello.sayHello( "Grey" ) ).isEqualTo( "HELLO GREY" );
         assertThat( hello.sayHi( "Grey" ) ).isEqualTo( "HI GREY" );
         assertThat( hello.sayThankYou( "Grey" ) ).isEqualTo( "THANK YOU GREY" );
+    }
+
+    @Test
+    public void dynamicProxy(){
+        Hello proxiedHello = (Hello) newProxyInstance(
+                getClass().getClassLoader(), // 동적으로 생성되는 다이내믹 프록시 클래스의 로딩에 사용할 클래스 로더
+                new Class[]{ Hello.class }, // 구현할 인터페이스
+                new UppercaseHandler( new HelloTarget() ) ); // 부가기능과 위임 코드를 담은 InvocationHandler
+
+        assertThat( proxiedHello.sayHello( "Grey" ) ).isEqualTo( "HELLO GREY" );
+        assertThat( proxiedHello.sayHi( "Grey" ) ).isEqualTo( "HI GREY" );
+        assertThat( proxiedHello.sayThankYou( "Grey" ) ).isEqualTo( "THANK YOU GREY" );
     }
 
 }
