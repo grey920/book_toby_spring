386042eaf8db247524746d9105b7f9c49fd6b735
grey <charminggw@gmail.com>
2024-02-21 10:29:43 +0900

    feat(proxy): UppercaseHandler 기능 확장
    1. 어떤 종류의 인터페이스를 구현한 타깃에도 적용 가능하도록 Object 타입을 주입받도록 수정
    2. 호출한 메소드의 리턴 타입이 String인 경우에만 toUpperCase()를 적용

--- src/test/java/org/springbook/learningtest/jdk/UppercaseHandler.java
+++ src/test/java/org/springbook/learningtest/jdk/UppercaseHandler.java
@@ -4,15 +4,26 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-    Hello target;
+    Object target;
 
-    public UppercaseHandler( Hello target ) {
+    /**
+     * 어떤 종류의 인터페이스를 구현한 타깃이 들어와도 적용 가능하도록 Object 타입을 받는다
+     * @param target
+     */
+    public UppercaseHandler( Object target ) {
         this.target = target;
     }
 
     @Override
     public Object invoke( Object proxy, Method method, Object[] args ) throws Throwable {
-        String ret = (String) method.invoke( target, args ); // 타깃으로 위임. 인터페이스의 메소드 호출에 모두 적용된다.
-        return ret.toUpperCase(); // 부가기능 제공
+        Object ret = method.invoke( target, args );
+        // 호출한 메소드의 리턴 타입이 String인 경우에만 대문자 변경 기능 적용
+        if ( ret instanceof String ) {
+            return ( ( String ) ret ).toUpperCase();
+        }
+        else {
+            return ret;
+        }
+
     }
 }
